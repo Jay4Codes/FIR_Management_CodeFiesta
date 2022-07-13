c56f48901858748b0510985f5375349d5fb8d5da
deep <patel.deep0731@gmail.com>
2022-07-13 11:06:22 +0530

    Added Procfile

--- codefiesta/codefiesta/settings.py
+++ codefiesta/codefiesta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5vy18tjdn(@#k5w=i3d&a$5v6a*sf*-&x0fuw&e+p@24%mcsda'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codeveista.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ALLOWED_ORIGINS = [
+    "https://codeveista.herokuapp.com/",
+    "http://localhost:8000",
+    "http://localhost:3000",
+
+]
