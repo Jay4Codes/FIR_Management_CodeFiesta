03064758a7625e2d8052ae3cf5ccc3d0079e171f
deep <patel.deep0731@gmail.com>
2022-07-13 11:18:52 +0530

    Deleted Files

--- codefiesta/codefiesta/settings.py
+++ codefiesta/codefiesta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5vy18tjdn(@#k5w=i3d&a$5v6a*sf*-&x0fuw&e+p@24%mcsda'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codeveista.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CORS_ALLOWED_ORIGINS = [
-    "https://codeveista.herokuapp.com/",
-    "http://localhost:8000",
-    "http://localhost:3000",
+# CORS_ALLOWED_ORIGINS = [
+#     "https://codeveista.herokuapp.com/",
+#     "http://localhost:8000",
+#     "http://localhost:3000",
 
-]
+# ]
